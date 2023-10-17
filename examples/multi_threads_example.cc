2be82d0d3491e5b2d47b67225a48cde434a2766b
petrizhang <petrizhang@tencent.com>
2023-10-17 11:31:32 +0800

    [Feature] Add interface to set omp_num_threads.

--- examples/multi_threads_example.cc
+++ examples/multi_threads_example.cc
@@ -29,6 +29,8 @@
 #include "tenann/factory/index_factory.h"
 #include "tenann/store/index_meta.h"
 #include "tenann/util/bruteforce_ann.h"
+#include "tenann/util/pretty_printer.h"
+#include "tenann/util/runtime_profile_macros.h"
 #include "tenann/util/threads.h"
 
 std::vector<float> RandomVectors(uint32_t n, uint32_t dim, int seed = 0) {
@@ -41,26 +43,7 @@ std::vector<float> RandomVectors(uint32_t n, uint32_t dim, int seed = 0) {
   return data;
 }
 
-void PrintResults(const std::vector<int64_t>& result_ids,
-                  const std::vector<float>& result_distances, int nq, int k) {
-  // search index
-  for (int i = 0; i < nq; i++) {
-    std::cout << "Result of query " << i << ": ";
-    for (int j = 0; j < k; j++) {
-      std::cout << result_ids[i * k + j] << ",";
-    }
-    std::cout << "\n";
-
-    for (int j = 0; j < k; j++) {
-      std::cout << result_distances[i * k + j] << ",";
-    }
-    std::cout << "\n";
-  }
-  std::cout << "-------------------------------------------------------------------\n";
-}
-
 int main() {
-  tenann::OmpSetNumThreads(8);
   tenann::IndexMeta meta;
 
   auto metric = tenann::MetricType::kCosineSimilarity;
@@ -71,15 +54,15 @@ int main() {
   meta.common_params()["dim"] = 128;
   meta.common_params()["is_vector_normed"] = false;
   meta.common_params()["metric_type"] = metric;
-  meta.index_params()["efConstruction"] = 500;
+  meta.index_params()["efConstruction"] = 40;
   meta.index_params()["M"] = 128;
-  meta.search_params()["efSearch"] = 80;
+  meta.search_params()["efSearch"] = 40;
   meta.extra_params()["comments"] = "my comments";
 
   // dimension of the vectors to index
   uint32_t d = 128;
   // size of the database we plan to index
-  size_t nb = 200;
+  size_t nb = 20000;
   // size of the query vectors we plan to test
   size_t nq = 10;
   // index save path
@@ -100,54 +83,40 @@ int main() {
                                        .dim = d,
                                        .size = static_cast<uint32_t>(nb),
                                        .elem_type = tenann::PrimitiveType::kFloatType};
-
-  T_LOG(WARNING) << "Generating query vectors...";
-  auto query = RandomVectors(nq, d, /*seed=*/1);
-  auto query_col = tenann::ArraySeqView{.data = reinterpret_cast<uint8_t*>(query.data()),
-                                        .dim = d,
-                                        .size = static_cast<uint32_t>(nq),
-                                        .elem_type = tenann::PrimitiveType::kFloatType};
-
   // build and write index
   auto index_builder1 = tenann::IndexFactory::CreateBuilderFromMeta(meta);
   tenann::IndexWriterRef index_writer = tenann::IndexFactory::CreateWriterFromMeta(meta);
+  auto index_builder2 = tenann::IndexFactory::CreateBuilderFromMeta(meta);
 
-  index_builder1->SetIndexWriter(index_writer)
-      .SetIndexCache(tenann::IndexCache::GetGlobalInstance())
-      .Open(index_path)
-      .Add({base_col})
-      .Flush(/*write_index_cache=*/true);
+  auto profile = std::make_unique<tenann::RuntimeProfile>("root");
+  auto single_thread_timer = T_ADD_TIMER(profile, "single");
+  auto multi_thread_timer = T_ADD_TIMER(profile, "multi");
 
-  meta.search_params()["efSearch"] = 900;
-  tenann::IndexReaderRef index_reader = tenann::IndexFactory::CreateReaderFromMeta(meta);
-  auto ann_searcher = tenann::AnnSearcherFactory::CreateSearcherFromMeta(meta);
-
-  // load index from disk file
-  ann_searcher->SetIndexReader(index_reader)
-      .SetIndexCache(tenann::IndexCache::GetGlobalInstance())
-      .ReadIndex(index_path, /*read_index_cache=*/true);
-  T_DCHECK(ann_searcher->is_index_loaded());
-
-  int k = 10;
-  std::vector<int64_t> result_ids(nq * k);
-  std::vector<float> result_distances(nq * k);
-
-  // search index
-  for (int i = 0; i < nq; i++) {
-    auto query_view =
-        tenann::PrimitiveSeqView{.data = reinterpret_cast<uint8_t*>(query.data() + i * d),
-                                 .size = d,
-                                 .elem_type = tenann::PrimitiveType::kFloatType};
-    ann_searcher->AnnSearch(query_view, k, result_ids.data() + i * k,
-                            reinterpret_cast<uint8_t*>(result_distances.data() + i * k));
+  T_LOG(WARNING) << "Build with single thread...";
+  tenann::OmpSetNumThreads(1);
+  {
+    T_SCOPED_TIMER(single_thread_timer);
+    index_builder1->SetIndexWriter(index_writer)
+        .SetIndexCache(tenann::IndexCache::GetGlobalInstance())
+        .Open(index_path)
+        .Add({base_col});
   }
 
-  std::cout << "HNSW Results: \n";
-  PrintResults(result_ids, result_distances, nq, k);
+  T_LOG(WARNING) << "Build with 8 threads...";
+  tenann::OmpSetNumThreads(8);
+  {
+    T_SCOPED_TIMER(multi_thread_timer);
+    index_builder2->SetIndexWriter(index_writer)
+        .SetIndexCache(tenann::IndexCache::GetGlobalInstance())
+        .Open(index_path)
+        .Add({base_col});
+  }
 
-  // brute force
-  tenann::util::BruteForceAnn(d, base_col, nullptr, nullptr, query_col, metric, k,
-                              result_ids.data(), result_distances.data());
-  std::cout << "Bruteforce Results: \n";
-  PrintResults(result_ids, result_distances, nq, k);
+  std::cout << "Build with single thread: "
+            << tenann::PrettyPrinter::print(single_thread_timer->value(), tenann::TUnit::TIME_NS)
+            << "\n";
+
+  std::cout << "Build with 8 threads: "
+            << tenann::PrettyPrinter::print(multi_thread_timer->value(), tenann::TUnit::TIME_NS)
+            << "\n";
 }
--- tenann/util/runtime_profile_macros.h
+++ tenann/util/runtime_profile_macros.h
@@ -20,6 +20,11 @@
 // Define macros for updating counters.  The macros make it very easy to disable
 // all counters at compile time.  Set this to 0 to remove counters.  This is useful
 // to do to make sure the counters aren't affecting the system.
+
+#pragma once
+
+#include "tenann/util/runtime_profile.h"
+
 #define T_ENABLE_COUNTERS 1
 
 // Some macro magic to generate unique ids using __COUNTER__
@@ -30,51 +35,58 @@
 #define T_ADD_COUNTER(profile, name, type) \
   (profile) == nullptr                     \
       ? nullptr                            \
-      : (profile)->add_counter(name, type, RuntimeProfile::Counter::create_strategy(type))
+      : (profile)->add_counter(name, type, tenann::RuntimeProfile::Counter::create_strategy(type))
 
 #define T_ADD_COUNTER_SKIP_MERGE(profile, name, type, merge_type) \
   (profile) == nullptr                                            \
       ? nullptr                                                   \
       : (profile)->add_counter(name, type,                        \
-                               RuntimeProfile::Counter::create_strategy(type, merge_type))
+                               tenann::RuntimeProfile::Counter::create_strategy(type, merge_type))
 
-#define T_ADD_TIMER(profile, name)                   \
-  (profile) == nullptr                               \
-      ? nullptr                                      \
-      : (profile)->add_counter(name, TUnit::TIME_NS, \
-                               RuntimeProfile::Counter::create_strategy(TUnit::TIME_NS))
+#define T_ADD_TIMER(profile, name)        \
+  (profile) == nullptr                    \
+      ? nullptr                           \
+      : (profile)->add_counter(           \
+            name, tenann::TUnit::TIME_NS, \
+            tenann::RuntimeProfile::Counter::create_strategy(tenann::TUnit::TIME_NS))
 
 #define T_ADD_CHILD_COUNTER(profile, name, type, parent) \
-  (profile) == nullptr ? nullptr                         \
-                       : (profile)->add_child_counter(   \
-                             name, type, RuntimeProfile::Counter::create_strategy(type), parent)
+  (profile) == nullptr                                   \
+      ? nullptr                                          \
+      : (profile)->add_child_counter(                    \
+            name, type, tenann::RuntimeProfile::Counter::create_strategy(type), parent)
 
-#define T_ADD_CHILD_COUNTER_SKIP_MERGE(profile, name, type, merge_type, parent) \
-  (profile) == nullptr                                                          \
-      ? nullptr                                                                 \
-      : (profile)->add_child_counter(                                           \
-            name, type, RuntimeProfile::Counter::create_strategy(type, merge_type), parent)
+#define T_ADD_CHILD_COUNTER_SKIP_MERGE(profile, name, type, merge_type, parent)             \
+  (profile) == nullptr                                                                      \
+      ? nullptr                                                                             \
+      : (profile)->add_child_counter(                                                       \
+            name, type, tenann::RuntimeProfile::Counter::create_strategy(type, merge_type), \
+            parent)
 
-#define T_ADD_CHILD_TIMER_THRESHOLD(profile, name, parent, threshold)                              \
-  (profile) == nullptr                                                                             \
-      ? nullptr                                                                                    \
-      : (profile)->add_child_counter(name, TUnit::TIME_NS,                                         \
-                                     RuntimeProfile::Counter::create_strategy(                     \
-                                         TUnit::TIME_NS, TCounterMergeType::MERGE_ALL, threshold), \
-                                     parent)
+#define T_ADD_CHILD_TIMER_THRESHOLD(profile, name, parent, threshold)                     \
+  (profile) == nullptr                                                                    \
+      ? nullptr                                                                           \
+      : (profile)->add_child_counter(                                                     \
+            name, tenann::TUnit::TIME_NS,                                                 \
+            tenann::RuntimeProfile::Counter::create_strategy(                             \
+                tenann::TUnit::TIME_NS, tenann::TCounterMergeType::MERGE_ALL, threshold), \
+            parent)
 
-#define T_ADD_CHILD_TIMER(profile, name, parent)       \
-  (profile) == nullptr ? nullptr                       \
-                       : (profile)->add_child_counter( \
-                             name, TUnit::TIME_NS,     \
-                             RuntimeProfile::Counter::create_strategy(TUnit::TIME_NS), parent)
+#define T_ADD_CHILD_TIMER(profile, name, parent) \
+  (profile) == nullptr                           \
+      ? nullptr                                  \
+      : (profile)->add_child_counter(            \
+            name, tenann::TUnit::TIME_NS,        \
+            tenann::RuntimeProfile::Counter::create_strategy(TUnit::TIME_NS), parent)
 
 #define T_SCOPED_TIMER(c) \
-  ScopedTimer<MonotonicStopWatch> T_MACRO_CONCAT(T_SCOPED_TIMER, __COUNTER__)(c)
-#define T_CANCEL_SAFE_SCOPED_TIMER(c, is_cancelled) \
-  ScopedTimer<MonotonicStopWatch> T_MACRO_CONCAT(T_SCOPED_TIMER, __COUNTER__)(c, is_cancelled)
-#define T_SCOPED_RAW_TIMER(c) \
-  ScopedRawTimer<MonotonicStopWatch> T_MACRO_CONCAT(T_SCOPED_RAW_TIMER, __COUNTER__)(c)
+  tenann::ScopedTimer<tenann::MonotonicStopWatch> T_MACRO_CONCAT(T_SCOPED_TIMER, __COUNTER__)(c)
+#define T_CANCEL_SAFE_SCOPED_TIMER(c, is_cancelled)                                            \
+  tenann::ScopedTimer<tenann::MonotonicStopWatch> T_MACRO_CONCAT(T_SCOPED_TIMER, __COUNTER__)( \
+      c, is_cancelled)
+#define T_SCOPED_RAW_TIMER(c)                                                           \
+  tenann::ScopedRawTimer<tenann::MonotonicStopWatch> T_MACRO_CONCAT(T_SCOPED_RAW_TIMER, \
+                                                                    __COUNTER__)(c)
 #define T_COUNTER_UPDATE(c, v) \
   if ((c) != nullptr) {        \
     (c)->update(v);            \
