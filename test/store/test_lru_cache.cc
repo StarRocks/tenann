4052dec3c220a55b40c96b9a7e6ac039d8b46fdc
eddyxiong <eddyxiong@tencent.com>
2023-11-20 09:14:54 +0000

    fix

--- test/store/test_lru_cache.cc
+++ test/store/test_lru_cache.cc
@@ -22,97 +22,6 @@
 #include "test/faiss_test_base.h"
 
 namespace tenann {
-// TEST(ShardedLRUCacheTest, InsertLookupAndRelease) {
-//   ShardedLRUCache cache(1000);
-//   Cache::Handle* handle;
-//   CacheKey key = "key1";
-//   std::string value = "value1";
-
-//   // 插入一个新的缓存条目
-//   handle = cache.insert(key, &value, value.size(), [](const CacheKey&, void*) {});
-//   ASSERT_NE(handle, nullptr);
-
-//   // 查找刚刚插入的缓存条目
-//   handle = cache.lookup(key);
-//   ASSERT_NE(handle, nullptr);
-//   EXPECT_EQ(*static_cast<std::string*>(cache.value(handle)), value);
-
-//   // 释放缓存条目
-//   cache.release(handle);
-// }
-
-// TEST(ShardedLRUCacheTest, Erase) {
-//   ShardedLRUCache cache(1000);
-//   Cache::Handle* handle;
-//   CacheKey key = "key1";
-//   std::string value = "value1";
-
-//   // 插入一个新的缓存条目
-//   handle = cache.insert(key, &value, value.size(), [](const CacheKey&, void*) {});
-//   ASSERT_NE(handle, nullptr);
-
-//   // 擦除刚刚插入的缓存条目
-//   cache.erase(key);
-
-//   // 查找擦除的缓存条目应该返回 nullptr
-//   handle = cache.lookup(key);
-//   EXPECT_EQ(handle, nullptr);
-// }
-
-// TEST(ShardedLRUCacheTest, NewId) {
-//   ShardedLRUCache cache(1000);
-//   uint64_t id1 = cache.new_id();
-//   uint64_t id2 = cache.new_id();
-
-//   // 新生成的 ID 应该是递增的
-//   EXPECT_LT(id1, id2);
-// }
-
-// TEST(ShardedLRUCacheTest, AdjustCapacity) {
-//   ShardedLRUCache cache(1000);
-//   CacheKey key = "key1";
-//   std::string value = "value1";
-//   Cache::Handle* handle;
-
-//   // 插入一个新的缓存条目
-//   handle = cache.insert(key, &value, value.size(), [](const CacheKey&, void*) {});
-//   ASSERT_NE(handle, nullptr);
-
-//   // 增加缓存容量
-//   cache.adjust_capacity(500);
-//   EXPECT_EQ(cache.get_capacity(), 1500);
-
-//   // 减少缓存容量
-//   cache.adjust_capacity(-500, 500);
-//   EXPECT_EQ(cache.get_capacity(), 1000);
-// }
-
-// TEST(ShardedLRUCacheTest, CapacityOverflow) {
-//   ShardedLRUCache cache(100);
-//   CacheKey key1 = "key1";
-//   std::string value1 = "value1";
-//   CacheKey key2 = "key2";
-//   std::string value2 = "value2";
-//   Cache::Handle* handle1;
-//   Cache::Handle* handle2;
-
-//   // 插入一个新的缓存条目，使缓存容量达到最大
-//   handle1 = cache.insert(key1, &value1, value1.size(), [](const CacheKey&, void*) {});
-//   ASSERT_NE(handle1, nullptr);
-//   // TODO: fix
-//   // EXPECT_EQ(cache.get_memory_usage(), 100);
-
-//   // 再插入一个新的缓存条目，此时缓存容量已满，应该会淘汰一些旧的缓存条目
-//   handle2 = cache.insert(key2, &value2, value2.size(), [](const CacheKey&, void*) {});
-//   ASSERT_NE(handle2, nullptr);
-//   // TODO: fix
-//   // EXPECT_EQ(cache.get_memory_usage(), 100);
-
-//   // 释放缓存条目
-//   cache.release(handle1);
-//   cache.release(handle2);
-// }
-
 class ShardedLRUCacheTest : public FaissTestBase {
  protected:
   void SetUp() override {
@@ -216,23 +125,6 @@ TEST_F(ShardedLRUCacheTest, SetCapacity) {
   EXPECT_EQ(cache_->get_capacity(), 2000);
 }
 
-TEST_F(ShardedLRUCacheTest, GetMemoryUsage) {
-  // Test getting the memory usage.
-  auto key = tenann::CacheKey{std::string("test_key")};
-  auto value = new int(42);
-  auto deleter = [](const tenann::CacheKey& key, void* value) {
-    delete static_cast<int*>(value);
-  };
-  auto handle = cache_->insert(key, value, sizeof(int), deleter);
-
-  EXPECT_EQ(cache_->get_memory_usage(), sizeof(int));
-}
-
-TEST_F(ShardedLRUCacheTest, GetCapacity) {
-  // Test getting the cache capacity.
-  EXPECT_EQ(cache_->get_capacity(), 1000);
-}
-
 TEST_F(ShardedLRUCacheTest, GetLookupCount) {
   // Test getting the lookup count.
   auto key = tenann::CacheKey{std::string("test_key")};
@@ -242,33 +134,14 @@ TEST_F(ShardedLRUCacheTest, GetLookupCount) {
   };
   auto handle = cache_->insert(key, value, sizeof(int), deleter);
 
-  cache_->lookup(key);
-  EXPECT_EQ(cache_->get_lookup_count(), 1);
-}
+  EXPECT_EQ(cache_->get_memory_usage(), sizeof(int));
 
-TEST_F(ShardedLRUCacheTest, GetHitCount) {
-  // Test getting the hit count.
-  auto key = tenann::CacheKey{std::string("test_key")};
-  auto value = new int(42);
-  auto deleter = [](const tenann::CacheKey& key, void* value) {
-    delete static_cast<int*>(value);
-  };
-  auto handle = cache_->insert(key, value, sizeof(int), deleter);
-
-  cache_->lookup(key);
-  EXPECT_EQ(cache_->get_hit_count(), 1);
-}
-
-TEST_F(ShardedLRUCacheTest, AdjustCapacity) {
-  // Test adjusting the cache capacity.
-  auto key = tenann::CacheKey{std::string("test_key")};
-  auto value = new int(42);
-  auto deleter = [](const tenann::CacheKey& key, void* value) {
-    delete static_cast<int*>(value);
-  };
-  auto handle = cache_->insert(key, value, sizeof(int), deleter);
-
+  EXPECT_EQ(cache_->get_capacity(), 1000);
   EXPECT_TRUE(cache_->adjust_capacity(500));
   EXPECT_EQ(cache_->get_capacity(), 1500);
+
+  cache_->lookup(key);
+  EXPECT_EQ(cache_->get_lookup_count(), 1);
+  EXPECT_EQ(cache_->get_hit_count(), 1);
 }
 }  // namespace tenann
